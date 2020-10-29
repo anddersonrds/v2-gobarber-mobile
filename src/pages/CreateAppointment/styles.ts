99ad45cf818a186404b46e616768ddd6495b8965
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-29 11:08:24 -0300

    add header in CreateAppointment page

--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,5 +1,31 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  flex: 1;
+`;
 
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  background: #28262e;
+`;
+export const BackButton = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text`
+  font-size: 20px;
+  font-family: 'RobotoSlab-Medium';
+  color: #f5ede8;
+  margin-left: 16px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  background: #999591;
+  margin-left: auto;
 `;
